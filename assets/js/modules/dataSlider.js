d7e6531ca9107c6b55e8054f0f9be82c2c001daa
elmira <elledevelope@gmail.com>
2024-10-14 16:48:02 +0200

    Created dataSlider

---
+++ assets/js/modules/dataSlider.js
@@ -0,0 +1,19 @@
+const dataSlider = [
+    {
+        urlImg: "https://picsum.photos/id/11/200/300",
+        title: "My title",
+        textContent: "lorem ipsum"
+    },
+    {
+        urlImg: "https://picsum.photos/id/152/200/300",
+        title: "My title",
+        textContent: "lorem ipsum"
+    },
+    {
+        urlImg: "https://picsum.photos/id/211/200/300",
+        title: "My title",
+        textContent: "lorem ipsum"
+    }
+];
+
+export { dataSlider };
---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+import { dataSlider } from "./modules/dataSlider.js";
+console.dir(dataSlider);
+console.log(dataSlider[0].title);

052c6413b9c57cfe79dfff52c3e4da5fa998bdda
elmira <elledevelope@gmail.com>
2024-10-17 15:26:50 +0200

    Created globalThis currentImg counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 import { dataSlider } from "./modules/dataSlider.js";
-console.dir(dataSlider);
-console.log(dataSlider[0].title);
-
 import { initImg } from "./modules/intImg.js";
+import { initNavigation } from "./modules/initNavigation.js";
+// console.dir(dataSlider);
+// console.log(dataSlider[0].title);
+
 
 globalThis.carousel = document.querySelector("#carousel");
+globalThis.dataSlider = dataSlider;
+globalThis.currentImg = 0;
+
+
 initImg();
+initNavigation();

793e183ddaed229f710962b8f9374c607b6da289
elmira <elledevelope@gmail.com>
2024-10-17 15:25:42 +0200

    Created fixed and move img in initImg.js

--- assets/js/modules/intImg.js
+++ assets/js/modules/intImg.js
@@ -1,5 +1,19 @@
 const initImg = () => {
-    console.dir(carousel);
+    // console.dir(carousel);
+    const fixedImage = document.createElement("img");
+    fixedImage.classList.add("fixedImg");
+    fixedImage.alt = "Image slider";
+    fixedImage.src = dataSlider[currentImg].urlImg;
+    carousel.append(fixedImage);
+
+    const moveImage = document.createElement("img");
+    moveImage.classList.add("moveImg");
+    moveImage.alt = "Image slider";
+    moveImage.src = dataSlider[currentImg].urlImg;
+    carousel.append(moveImage);
+
+    fixedImage.width = moveImage.width = 300;
+    fixedImage.height = moveImage.height = 300;
 };
 
 export { initImg };

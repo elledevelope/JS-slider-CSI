bd44ce1a9f649b84420de1b7538f88c96646776c
elmira <elledevelope@gmail.com>
2024-10-17 15:41:56 +0200

    Add caret navigation in module initNavigation.js

--- assets/js/modules/initNavigation.js
+++ assets/js/modules/initNavigation.js
@@ -1,6 +1,17 @@
-const initNavigation = () => {
-    const divNext = document.createElement("div");
+const createNavBtn = (className, faClass) => {
+    const div = document.createElement("div");
+    div.classList.add(className);
+    const i = document.createElement("i");
+    i.classList.add(faClass);
+    i.classList.add("fa-solid");
+    carousel.append(div);
+    div.append(i);
+    return div;
+};
 
+const initNavigation = () => {
+    const divNext = createNavBtn("divNext", "fa-caret-right");
+    const divPrev = createNavBtn("divPrev", "fa-caret-left");
 };
 
 export { initNavigation };
